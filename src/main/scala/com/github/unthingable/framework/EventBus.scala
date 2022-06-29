c74d1ef4bf67c6d2ec27355dc27e0e062b432a09
unthingable <unthingable@users.noreply.github.com>
2022-06-29 10:11:46 +0200

    event bus: guarantee evaluation order

--- src/main/scala/com/github/unthingable/framework/EventBus.scala
+++ src/main/scala/com/github/unthingable/framework/EventBus.scala
@@ -5,23 +5,46 @@ import com.github.unthingable.jam.surface.WithSource
 
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
-
-/**
- * Stupid simple pubsub
- */
+import scala.annotation.targetName
 
 class EventBus[E] {
   type Reactor = E => Unit
+  case class EvtContext[E](e: E, ctx: String)
 
   private val subs = mutable.HashMap.empty[E, mutable.HashSet[Reactor]]
 
-  def eval(e: E*): Unit =
-    for (ev <- e)
-      val receivers = subs.get(ev).toSeq.flatten
-      if (receivers.nonEmpty) Util.println(s"evt: $e => ${receivers.size}")
-      receivers.foreach(_(ev))
+  /* Janky actor with a queue, no mind paid to concurrency.
+     Assuming the extention is single-threaded this should be fine.
+   */
+  private val queue = mutable.Queue.empty[EvtContext[E]]
+  private var semaphore: Boolean = false
 
-  def eval(e: WithSource[E, _]): Unit = eval(e.value)
+  private def trigger(): Unit =
+    if !semaphore then
+      semaphore = true
+      evalQueue()
+
+  private def evalQueue(): Unit = 
+    while (semaphore && queue.nonEmpty)
+      val e = queue.dequeue()
+      val ev = e.e
+      Util.println(s"evt: $e")
+      val receivers = subs.get(ev).toSeq.flatten
+      if (receivers.nonEmpty) Util.println(s"evt: $ev => ${receivers.size} ${e.ctx}")
+      receivers.foreach(_(ev))
+    semaphore = false
+
+  def eval(context: String, e: E*): Unit =
+    queue.enqueueAll(e.map(EvtContext(_, context)))
+    trigger()
+
+  def eval(e: E*): Unit = eval("", e*)
+
+  @targetName("evalS")
+  def eval(e: WithSource[E, _]*): Unit = eval(e.map(_.value)*)
+
+  @targetName("evalS")
+  def eval(context: String, e: WithSource[E, _]*): Unit = eval(context, e.map(_.value)*)
 
   def addSub(e: E, r: Reactor): Unit =
     getSub(e).addOne(r)
