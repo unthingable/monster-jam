1937e89c7e48892557d1561f4777098d21f8b3ba
unthingable <unthingable@users.noreply.github.com>
2022-08-18 11:26:05 +0200

    WIP event subscription by class

--- src/main/scala/com/github/unthingable/framework/EventBus.scala
+++ src/main/scala/com/github/unthingable/framework/EventBus.scala
@@ -6,17 +6,23 @@ import com.github.unthingable.jam.surface.WithSource
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 import scala.annotation.targetName
+import scala.reflect.ClassTag
 
-class EventBus[E] {
-  type Reactor = E => Unit
-  case class EvtContext[E](e: E, ctx: String)
+class EventBus[BaseE] {
+  type Reactor[E <: BaseE] = E => Unit
+  type EvtMatcher[+E <: BaseE] = E | Class[_]
+  case class EvtContext[+E <: BaseE](e: E, ctx: String)
 
-  private val subs = mutable.HashMap.empty[E, mutable.HashSet[Reactor]]
+  private val valueSubs = mutable.HashMap.empty[BaseE, mutable.HashSet[Reactor[_]]]
+  private val classSubs = mutable.HashMap.empty[Class[_], mutable.HashSet[Reactor[_]]]
 
+  // private val Subs = mutable.HashMap.empty[Class[? <: BaseE], mutable.HashSet[Reactor[_]]]
+
+  
   /* Janky actor with a queue, no mind paid to concurrency.
      Assuming the extention is single-threaded this should be fine.
    */
-  private val queue              = mutable.Queue.empty[EvtContext[E]]
+  private val queue              = mutable.Queue.empty[EvtContext[BaseE]]
   private var semaphore: Boolean = false
 
   private def trigger(): Unit =
@@ -24,50 +30,60 @@ class EventBus[E] {
       semaphore = true
       evalQueue(queue)
 
-  private def evalQueue(queue: mutable.Queue[EvtContext[E]]): Unit =
+  private def evalQueue(queue: mutable.Queue[EvtContext[BaseE]]): Unit =
     while (semaphore && queue.nonEmpty)
       val e  = queue.dequeue()
       val ev = e.e
       // Util.println(s"evt: $e")
-      val receivers = subs.get(ev).toSeq.flatten
+      val receivers = receiversFor(ev) // valueSubs.get(ev).toSeq.flatten
       if (receivers.nonEmpty) Util.println(s"evt: $ev => ${receivers.size} ${e.ctx}")
       receivers.foreach(_(ev))
     semaphore = false
 
-  def eval(context: String)(e: E*): Unit =
+  def eval(context: String)(e: BaseE*): Unit =
     Util.println(s"evt: enqueueing $e with $context")
     queue.enqueueAll(e.map(EvtContext(_, context)))
     trigger()
 
   // jump the line
-  def evalNow(context: String)(e: E*): Unit =
+  def evalNow(context: String)(e: BaseE*): Unit =
     Util.println(s"evt: prequeueing $e with $context")
     e.foreach(ev =>
-      val receivers = subs.get(ev).toSeq.flatten
+      val receivers = receiversFor(ev) // valueSubs.get(ev).toSeq.flatten
       if (receivers.nonEmpty) Util.println(s"evt: $ev => ${receivers.size} ${context}")
       receivers.foreach(_(ev))
     )
 
-  private def eval(e: E*): Unit = eval("")(e*)
+  private def eval(e: BaseE*): Unit = eval("")(e*)
 
   @targetName("evalS")
-  private def eval(e: WithSource[E, _]*): Unit = eval(e.map(_.value)*)
+  private def eval(e: WithSource[BaseE, _]*): Unit = eval(e.map(_.value)*)
 
   @targetName("evalS")
-  def eval(context: String)(e: WithSource[E, _]*): Unit = eval(context)(e.map(_.value)*)
+  def eval(context: String)(e: WithSource[BaseE, _]*): Unit = eval(context)(e.map(_.value)*)
 
-  def addSub(e: E, r: Reactor): Unit =
+  inline def addSub[E <: BaseE](e: E, r: Reactor[E]): Unit =
     getSub(e).addOne(r)
 
-  def rmSub(e: E, r: Reactor): Unit =
+  inline def addSub[E <: BaseE](r: Reactor[E])(using ct: ClassTag[E]): Unit =
+    getSub(ct.runtimeClass).addOne(r)
+
+  inline def rmSub[E <: BaseE](e: EvtMatcher[E], r: Reactor[E]): Unit =
     getSub(e).filterInPlace(_ != r)
 
-  def setSub(e: E, r: Reactor): Unit =
-    subs.update(e, mutable.HashSet(r))
+  def setSub[E <: BaseE](e: E, r: Reactor[E]): Unit =
+    valueSubs.update(e, mutable.HashSet(r))
 
-  def clearSub(e: E): Unit =
-    subs.remove(e)
+  def clearSub(e: BaseE): Unit =
+    valueSubs.remove(e)
 
-  private def getSub(e: E): mutable.HashSet[Reactor] =
-    subs.getOrElseUpdate(e, mutable.HashSet.empty[Reactor])
+  inline def getSub[E <: BaseE](e: EvtMatcher[E]): mutable.HashSet[Reactor[E]] =
+    inline e match
+      case ev: E => valueSubs.getOrElseUpdate(ev, mutable.HashSet.empty[Reactor[_]]).asInstanceOf[mutable.HashSet[Reactor[E]]]
+      case ec: Class[_] => classSubs.getOrElseUpdate(ec, mutable.HashSet.empty[Reactor[_]]).asInstanceOf[mutable.HashSet[Reactor[E]]]
+
+  private def receiversFor[E <: BaseE](e: E): Iterable[Reactor[E]] =
+    (valueSubs.get(e).toSeq.flatten 
+    ++ classSubs.get(e.getClass()).toSeq.flatten)
+    .asInstanceOf[Iterable[Reactor[E]]]
 }
