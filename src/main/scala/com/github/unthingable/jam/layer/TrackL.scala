743d07f7992cedd6025e004c7cb4a2b8a26e85ce
unthingable <unthingable@users.noreply.github.com>
2022-09-22 00:47:52 +0200

    format TrackL

--- src/main/scala/com/github/unthingable/jam/layer/TrackL.scala
+++ src/main/scala/com/github/unthingable/jam/layer/TrackL.scala
@@ -35,8 +35,8 @@ trait TrackL { this: Jam =>
         val now = Instant.now()
         if (track.isGroup.get && now.isBefore(pressedOn.plusMillis(400))) {
 
-          //val trackId  = tracker.trackId(track)
-          //val callback = () => {
+          // val trackId  = tracker.trackId(track)
+          // val callback = () => {
           //  for {
           //    id <- trackId
           //    pos <- tracker.positionForId(id)
@@ -45,80 +45,88 @@ trait TrackL { this: Jam =>
           //    trackBank.scrollPosition().set(pos - idx)
           //  }
           //  ()
-          //}
+          // }
           //
-          //tracker.addRescanCallback(callback)
+          // tracker.addRescanCallback(callback)
 
           track.isGroupExpanded.toggle()
-          //foldToggleTop.invoke()
+          // foldToggleTop.invoke()
 
         } else if (GlobalMode.Clear.isOn) track.deleteObject()
-               else if (GlobalMode.Duplicate.isOn) track.duplicate()
-                    else track.selectInMixer()
+        else if (GlobalMode.Duplicate.isOn) track.duplicate()
+        else track.selectInMixer()
         pressedOn = now
       }
 
       Vector(
-        SupColorStateB(btn.light, () => JamColorState(
-          color.get(),
-          brightness = (playingNotes.get().length > 0, cursorIndex.get() == idx) match {
-            case (_, true)  => 3
-            case (true, _)  => 2
-            case (false, _) => 0
-          }
-        ), JamColorState.empty),
-        //HB(btn.button.press(), () => trackBank.cursorIndex().set(idx))
+        SupColorStateB(
+          btn.light,
+          () =>
+            JamColorState(
+              color.get(),
+              brightness = (playingNotes.get().length > 0, cursorIndex.get() == idx) match {
+                case (_, true)  => 3
+                case (true, _)  => 2
+                case (false, _) => 0
+              }
+            ),
+          JamColorState.empty
+        ),
+        // HB(btn.button.press(), () => trackBank.cursorIndex().set(idx))
         EB(btn.st.press, s"group $idx pressed: select in mixer", () => handlePress())
       )
     }
   }
 
-  def trackGate(idx: Int) = new ModeButtonLayer(s"track gate $idx",
-    j.groupButtons(idx),
-    GateMode.Gate,
-    silent = true
-  ) {
-    import com.github.unthingable.jam.surface.KeyMaster._
-    val track   = trackBank.getItemAt(idx)
-    val isAtTop = ext.host.getProject.getRootTrackGroup.createEqualsValue(ext.host.getProject.getShownTopLevelTrackGroup)
+  def trackGate(idx: Int) =
+    new ModeButtonLayer(s"track gate $idx", j.groupButtons(idx), GateMode.Gate, silent = true) {
+      import com.github.unthingable.jam.surface.KeyMaster._
+      val track = trackBank.getItemAt(idx)
+      val isAtTop = ext.host.getProject.getRootTrackGroup
+        .createEqualsValue(ext.host.getProject.getShownTopLevelTrackGroup)
 
-    track.isGroup.markInterested()
-    isAtTop.markInterested()
+      track.isGroup.markInterested()
+      isAtTop.markInterested()
 
-    track.mute().markInterested()
-    track.solo().markInterested()
-    track.arm().markInterested()
-    trackBank.scrollPosition().markInterested()
+      track.mute().markInterested()
+      track.solo().markInterested()
+      track.arm().markInterested()
+      trackBank.scrollPosition().markInterested()
 
-    /*
-    Workaround for weird TrackBank.scrollBy() behavior - if next page is empty it won't scroll right,
-    instead it will always jump to (last track index - bank size) [117008]
-     */
-    def scrollBy(n: Int): Unit = trackBank.scrollPosition().set(trackBank.scrollPosition().get() + n)
+      /*
+      Workaround for weird TrackBank.scrollBy() behavior - if next page is empty it won't scroll right,
+      instead it will always jump to (last track index - bank size) [117008]
+       */
+      def scrollBy(n: Int): Unit =
+        trackBank.scrollPosition().set(trackBank.scrollPosition().get() + n)
 
-    override val modeBindings: Seq[Binding[_, _, _]] = Vector(
-      SupBooleanB(j.dpad.up.light.isOn, () => !isAtTop.get() && j.Mod.blink3),
-      SupBooleanB(j.dpad.down.light.isOn, () => track.isGroup.get() && j.Mod.blink3),
-      SupBooleanB(j.dpad.left.light.isOn, () => true),
-      SupBooleanB(j.dpad.right.light.isOn, () => true),
-      EB(j.dpad.up.st.press, "exit group", () => ext.application.navigateToParentTrackGroup()),
-      EB(j.dpad.down.st.press, "enter group", () => ext.application.navigateIntoTrackGroup(track)),
-      EB(j.dpad.left.st.press, "scroll left", () => scrollBy(idx - 7)),
-      EB(j.dpad.right.st.press, "scroll right", () => scrollBy(idx)),
-      SupBooleanB(j.solo.light.isOn, track.solo()),
-      SupBooleanB(j.mute.light.isOn, track.mute()),
-      SupBooleanB(j.record.light.isOn, track.arm()),
-      // FIXME - fixed?
-      EB(j.solo.st.press, "track direct solo", () => track.solo().toggle()),
-      EB(j.mute.st.press, "track direct mute", () => track.mute().toggle),
-      EB(j.record.st.press, "track direct arm", () => track.arm().toggle()),
-    )
-  }
+      override val modeBindings: Seq[Binding[_, _, _]] = Vector(
+        SupBooleanB(j.dpad.up.light.isOn, () => !isAtTop.get() && j.Mod.blink3),
+        SupBooleanB(j.dpad.down.light.isOn, () => track.isGroup.get() && j.Mod.blink3),
+        SupBooleanB(j.dpad.left.light.isOn, () => true),
+        SupBooleanB(j.dpad.right.light.isOn, () => true),
+        EB(j.dpad.up.st.press, "exit group", () => ext.application.navigateToParentTrackGroup()),
+        EB(
+          j.dpad.down.st.press,
+          "enter group",
+          () => ext.application.navigateIntoTrackGroup(track)
+        ),
+        EB(j.dpad.left.st.press, "scroll left", () => scrollBy(idx - 7)),
+        EB(j.dpad.right.st.press, "scroll right", () => scrollBy(idx)),
+        SupBooleanB(j.solo.light.isOn, track.solo()),
+        SupBooleanB(j.mute.light.isOn, track.mute()),
+        SupBooleanB(j.record.light.isOn, track.arm()),
+        // FIXME - fixed?
+        EB(j.solo.st.press, "track direct solo", () => track.solo().toggle()),
+        EB(j.mute.st.press, "track direct mute", () => track.mute().toggle),
+        EB(j.record.st.press, "track direct arm", () => track.arm().toggle()),
+      )
+    }
 
   lazy val masterButton = new SimpleModeLayer("master button") {
-    val first : Track = trackBank.getItemAt(0)
-    val parent: Track = first.createParentTrack(0,0)
-    val master: MasterTrack = ext.host.createMasterTrack(0)
+    val first: Track               = trackBank.getItemAt(0)
+    val parent: Track              = first.createParentTrack(0, 0)
+    val master: MasterTrack        = ext.host.createMasterTrack(0)
     val equalsMaster: BooleanValue = master.createEqualsValue(ext.cursorTrack)
     val equalsParent: BooleanValue = parent.createEqualsValue(ext.cursorTrack)
 
