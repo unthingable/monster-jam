ebb6c3a9830e899de2ec0267962eea5e6ec0b73e
unthingable <unthingable@users.noreply.github.com>
2022-09-25 09:56:00 +0200

    format and inline StripBank

--- src/main/scala/com/github/unthingable/jam/surface/StripBank.scala
+++ src/main/scala/com/github/unthingable/jam/surface/StripBank.scala
@@ -1,23 +1,26 @@
 package com.github.unthingable.jam.surface
 
 import com.github.unthingable.{MonsterJamExt, Util}
-import com.github.unthingable.jam.surface.BlackSysexMagic.{BarMode, createCommand}
+import com.github.unthingable.jam.surface.BlackSysexMagic.{createCommand, BarMode}
 
 import scala.collection.mutable
 
 class StripBank()(implicit ext: MonsterJamExt) extends Util {
-  val strips: Vector[JamTouchStrip] = ('A' to 'H').map { idx =>
-    new JamTouchStrip(
-      touch = ext.xmlMap.button(s"CapTst$idx", ext.xmlMap.touchElems),
-      slide = ext.xmlMap.knob(s"Tst$idx", ext.xmlMap.touchElems),
-      led = ext.xmlMap.led(s"Tst${idx}IDX", ext.xmlMap.touchElems))
+  val strips: Vector[JamTouchStrip] = ('A' to 'H')
+    .map { idx =>
+      new JamTouchStrip(
+        touch = ext.xmlMap.button(s"CapTst$idx", ext.xmlMap.touchElems),
+        slide = ext.xmlMap.knob(s"Tst$idx", ext.xmlMap.touchElems),
+        led = ext.xmlMap.led(s"Tst${idx}IDX", ext.xmlMap.touchElems)
+      )
 
-  }.toVector
+    }
+    .toVector
     .forindex(_.slider.setIndexInGroup(_))
 
-  var barMode: Int => BarMode = _ => BarMode.DUAL
-  private val colors: mutable.ArraySeq[Int] = mutable.ArraySeq.fill(8)(0)
-  private val values: mutable.ArraySeq[Int] = mutable.ArraySeq.fill(8)(0)
+  var barMode: Int => BarMode                   = _ => BarMode.DUAL
+  private val colors: mutable.ArraySeq[Int]     = mutable.ArraySeq.fill(8)(0)
+  private val values: mutable.ArraySeq[Int]     = mutable.ArraySeq.fill(8)(0)
   private val active: mutable.ArraySeq[Boolean] = mutable.ArraySeq.fill(8)(false)
 
   def setColor(idx: Int, color: Int, flush: Boolean = true): Unit = {
@@ -25,28 +28,33 @@ class StripBank()(implicit ext: MonsterJamExt) extends Util {
     colors.update(idx, if (color == 68 || color == 0) 120 else color)
     if (flush) flushColors()
   }
-  def setValue(idx: Int, value: Int, flush: Boolean = true): Unit = {
+  inline def setValue(idx: Int, value: Int, inline flush: Boolean = true): Unit =
     if (barMode(idx) == BarMode.DUAL) {
       values.update(idx, value)
-      if (flush) flushValues()
+      inline if (flush) flushValues()
     } else strips(idx).update(value)
-  }
-  def setActive(idx: Int, value: Boolean, flush: Boolean = true): Unit = {
+  inline def setActive(inline idx: Int, inline value: Boolean, inline flush: Boolean = true): Unit =
     active.update(idx, value)
-    if (flush) flushColors()
-  }
+    inline if (flush) flushColors()
 
   inline def setActive(f: Int => Boolean): Unit =
     (0 until 8).foreach(idx => active.update(idx, f(idx)))
     flushColors()
 
-  def flushColors(): Unit =
-    ext.midiOut.sendSysex(createCommand("05",
-      colors.zip(active).zipWithIndex.map { case ((n, a), idx) => f"${if (a) barMode(idx).v else "00"}${n}%02x"}.mkString))
+  inline def flushColors(): Unit =
+    ext.midiOut.sendSysex(
+      createCommand(
+        "05",
+        colors
+          .zip(active)
+          .zipWithIndex
+          .map { case ((n, a), idx) => f"${if (a) barMode(idx).v else "00"}${n}%02x" }
+          .mkString
+      )
+    )
 
-  def flushValues(): Unit = {
+  inline def flushValues(): Unit =
     ext.midiOut.sendSysex(createCommand("04", values.map(n => f"${n}%02x").mkString))
-  }
 
-  def clear(): Unit = ext.midiOut.sendSysex(BlackSysexMagic.zeroStrips)
+  inline def clear(): Unit = ext.midiOut.sendSysex(BlackSysexMagic.zeroStrips)
 }
