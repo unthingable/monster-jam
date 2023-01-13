911b99f80fd320e55040094bef3aaf7104b9fe4f
unthingable <unthingable@users.noreply.github.com>
2023-01-14 00:10:32 +0100

    ver bump

--- src/main/scala/com/github/unthingable/MonsterJamControllerDefinition.scala
+++ src/main/scala/com/github/unthingable/MonsterJamControllerDefinition.scala
@@ -16,7 +16,7 @@ class MonsterJamExtensionDefinition() extends ControllerExtensionDefinition {
 
   override def getAuthor = "unthingable"
 
-  override def getVersion = "8.0-b11"
+  override def getVersion = "8.0-b12"
 
   override def getId: UUID = MonsterJamExtensionDefinition.DRIVER_ID
 
